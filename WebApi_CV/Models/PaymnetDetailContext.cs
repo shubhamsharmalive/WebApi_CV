3b8b2ed1a954ba7da003ec7bf34bfe22fd62dd44
Shubham Sharma <31825691+shubhamsharmalive@users.noreply.github.com>
2019-06-02 23:42:24 +0530

    Changes for MSSQL Cponnection and database creation.

---
+++ WebApi_CV/Models/PaymnetDetailContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_CV.Models
+{
+    public class PaymnetDetailContext : DbContext
+    {
+        public PaymnetDetailContext(DbContextOptions<PaymnetDetailContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> paymentDetails { get; set; }
+    }
+}
